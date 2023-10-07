ea47901622e84d8cad0a69350d40493adca4761c
Steven Velozo <steven@velozo.com>
2023-10-07 09:44:59 -0700

    Converged foundation classes for fable service types (core and noncore)

--- dist/fable-serviceproviderbase.js
+++ dist/fable-serviceproviderbase.js
@@ -51,62 +51,62 @@ function _toPrimitive(input, hint) { if (typeof input !== "object" || input === 
   }()({
     1: [function (require, module, exports) {
       /**
-      * Fable Core Pre-initialization Service Base
-      *
-      * For a couple services, we need to be able to instantiate them before the Fable object is fully initialized.
-      * This is a base class for those services.
-      *
-      * @author <steven@velozo.com>
-      */
-
-      class FableCoreServiceProviderBase {
-        constructor(pOptions, pServiceHash) {
-          this.fable = false;
-          this.options = typeof pOptions === 'object' ? pOptions : {};
-          this.serviceType = 'Unknown';
-
-          // The hash will be a non-standard UUID ... the UUID service uses this base class!
-          this.UUID = "CORESVC-".concat(Math.floor(Math.random() * (99999 - 10000) + 10000));
-          this.Hash = typeof pServiceHash === 'string' ? pServiceHash : "".concat(this.UUID);
-        }
-        // After fable is initialized, it would be expected to be wired in as a normal service.
-        connectFable(pFable) {
-          this.fable = pFable;
-          return true;
-        }
-      }
-      _defineProperty(FableCoreServiceProviderBase, "isFableService", true);
-      module.exports = FableCoreServiceProviderBase;
-    }, {}],
-    2: [function (require, module, exports) {
-      /**
       * Fable Service Base
       * @author <steven@velozo.com>
       */
 
       class FableServiceProviderBase {
+        // The constructor can be used in two ways:
+        // 1) With a fable, options object and service hash (the options object and service hash are optional)
+        // 2) With an object or nothing as the first parameter, where it will be treated as the options object
         constructor(pFable, pOptions, pServiceHash) {
-          this.fable = pFable;
-          this.options = typeof pOptions === 'object' ? pOptions : typeof pFable === 'object' && !pFable.isFable ? pFable : {};
+          // Check if a fable was passed in
+          if (typeof pFable === 'object' && pFable.isFable) {
+            this.fable = pFable;
+          } else {
+            this.fable = false;
+          }
+          if (this.fable) {
+            this.connectFable(this.fable);
+            this.options = typeof pOptions === 'object' ? pOptions : {};
+          } else {
+            // With no fable, check to see if there was an object passed into either of the first two
+            // Parameters, and if so, treat it as the options object
+            this.options = typeof pFable === 'object' && !pFable.isFable ? pFable : typeof pOptions === 'object' ? pOptions : {};
+          }
+
+          // It's expected that the deriving class will set this
           this.serviceType = 'Unknown';
-          if (typeof pFable.getUUID == 'function') {
+          if (this.fable) {
             this.UUID = pFable.getUUID();
           } else {
-            this.UUID = "NoFABLESVC-".concat(Math.floor(Math.random() * (99999 - 10000) + 10000));
+            // Without any dependencies, get a decently random UUID for the service
+            this.UUID = "CORE-SVC-".concat(Math.floor(Math.random() * (99999 - 10000) + 10000));
           }
-          this.Hash = typeof pServiceHash === 'string' ? pServiceHash : "".concat(this.UUID);
 
-          // Pull back a few things
+          // The service hash is used to identify the specific instantiation of the service in the services map
+          this.Hash = typeof pServiceHash === 'string' ? pServiceHash : !this.fable && typeof pOptions === 'string' ? pOptions : "".concat(this.UUID);
+        }
+        connectFable(pFable) {
+          if (typeof pFable !== 'object' || !pFable.isFable) {
+            let tmpErrorMessage = "Fable Service Provider Base: Cannot connect to Fable, invalid Fable object passed in.  The pFable parameter was a [".concat(typeof pFable, "].}");
+            console.log(tmpErrorMessage);
+            return new Error(tmpErrorMessage);
+          }
+          if (!this.fable) {
+            this.fable = pFable;
+          }
           this.log = this.fable.log;
           this.servicesMap = this.fable.servicesMap;
           this.services = this.fable.services;
+          return true;
         }
       }
       _defineProperty(FableServiceProviderBase, "isFableService", true);
       module.exports = FableServiceProviderBase;
-      module.exports.CoreServiceProviderBase = require('./Fable-ServiceProviderBase-Preinit.js');
-    }, {
-      "./Fable-ServiceProviderBase-Preinit.js": 1
-    }]
-  }, {}, [2])(2);
+
+      // This is left here in case we want to go back to having different code for "core" services
+      module.exports.CoreServiceProviderBase = FableServiceProviderBase;
+    }, {}]
+  }, {}, [1])(1);
 });
--- source/Fable-ServiceProviderBase.js
+++ source/Fable-ServiceProviderBase.js
@@ -5,31 +5,74 @@
 
 class FableServiceProviderBase
 {
+	// The constructor can be used in two ways:
+	// 1) With a fable, options object and service hash (the options object and service hash are optional)
+	// 2) With an object or nothing as the first parameter, where it will be treated as the options object
 	constructor(pFable, pOptions, pServiceHash)
 	{
-		this.fable = pFable;
+		// Check if a fable was passed in
+		if ((typeof(pFable) === 'object') && pFable.isFable)
+		{
+			this.fable = pFable;
+		}
+		else
+		{
+			this.fable = false;
+		}
 
-		this.options = (typeof(pOptions) === 'object') ? pOptions
-						: ((typeof(pFable) === 'object') && !pFable.isFable) ? pFable
-						: {};
+		if (this.fable)
+		{
+			this.connectFable(this.fable);
+			this.options = (typeof(pOptions) === 'object') ? pOptions
+							: {};
+		}
+		else
+		{
+			// With no fable, check to see if there was an object passed into either of the first two
+			// Parameters, and if so, treat it as the options object
+			this.options = ((typeof(pFable) === 'object') && !pFable.isFable) ? pFable
+							: (typeof(pOptions) === 'object') ? pOptions
+							: {};
+		}
 
+		// It's expected that the deriving class will set this
 		this.serviceType = 'Unknown';
 
-		if (typeof(pFable.getUUID) == 'function')
+		if (this.fable)
 		{
 			this.UUID = pFable.getUUID();
 		}
 		else
 		{
-			this.UUID = `NoFABLESVC-${Math.floor((Math.random() * (99999 - 10000)) + 10000)}`
+			// Without any dependencies, get a decently random UUID for the service
+			this.UUID = `CORE-SVC-${Math.floor((Math.random() * (99999 - 10000)) + 10000)}`
 		}
 
-		this.Hash = (typeof(pServiceHash) === 'string') ? pServiceHash : `${this.UUID}`;
+		// The service hash is used to identify the specific instantiation of the service in the services map
+		this.Hash = (typeof(pServiceHash) === 'string') ? pServiceHash 
+					: (!this.fable && (typeof(pOptions) === 'string')) ? pOptions
+					: `${this.UUID}`;
+	}
 
-		// Pull back a few things
+	connectFable(pFable)
+	{
+		if ((typeof(pFable) !== 'object') || (!pFable.isFable))
+		{
+			let tmpErrorMessage = `Fable Service Provider Base: Cannot connect to Fable, invalid Fable object passed in.  The pFable parameter was a [${typeof(pFable)}].}`;
+			console.log(tmpErrorMessage);
+			return new Error(tmpErrorMessage);
+		}
+
+		if (!this.fable)
+		{
+			this.fable = pFable;
+		}
+
 		this.log = this.fable.log;
 		this.servicesMap = this.fable.servicesMap;
 		this.services = this.fable.services;
+
+		return true;
 	}
 
 	static isFableService = true;
@@ -37,4 +80,5 @@ class FableServiceProviderBase
 
 module.exports = FableServiceProviderBase;
 
-module.exports.CoreServiceProviderBase = require('./Fable-ServiceProviderBase-Preinit.js');
+// This is left here in case we want to go back to having different code for "core" services
+module.exports.CoreServiceProviderBase = FableServiceProviderBase;
